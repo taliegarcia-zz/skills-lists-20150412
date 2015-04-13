b94bbe31d80c6aaae15e6142e29e9ae8aa2120c3
Natalie <taliegarcia@gmail.com>
2015-04-12 22:54:50 -0700

    wrote all_odd function

--- skills.py
+++ skills.py
@@ -2,7 +2,7 @@
 INTERMEDIATE = False
 
 # To work on the advanced problems, set to True
-ADVANCED = False
+ADVANCED = True
 
 
 def all_odd(number_list):
@@ -15,219 +15,223 @@ def all_odd(number_list):
         []
 
     """
+    odd_list = []
+    for num in number_list:
+        if num % 2 != 0:
+            odd_list.append(num)
 
-    return []
 
+    return odd_list
 
-def all_even(number_list):
-    """Return a list of only the even numbers in the input list.
+# def all_even(number_list):
+#     """Return a list of only the even numbers in the input list.
 
-        >>> all_even([2, 6, -1, -2])
-        [2, 6, -2]
+#         >>> all_even([2, 6, -1, -2])
+#         [2, 6, -2]
 
-        >>> all_even([-1, 3, 5])
-        []
+#         >>> all_even([-1, 3, 5])
+#         []
 
-    """
+#     """
 
-    return []
+#     return []
 
 
-def print_indeces(my_list):
-    """Print the index of each list item, followed by the item itself.
-    Do this without using a counting variable. I.e. don't do something
-    like this:
+# def print_indeces(my_list):
+#     """Print the index of each list item, followed by the item itself.
+#     Do this without using a counting variable. I.e. don't do something
+#     like this:
 
-    count = 0
-    for item in list:
-        print count
-        count = count + 1
+#     count = 0
+#     for item in list:
+#         print count
+#         count = count + 1
 
-    Output should look like this:
+#     Output should look like this:
 
-    >>> print_indeces(["Toyota", "Jeep", "Volvo"])
-    0 Toyota
-    1 Jeep
-    2 Volvo
+#     >>> print_indeces(["Toyota", "Jeep", "Volvo"])
+#     0 Toyota
+#     1 Jeep
+#     2 Volvo
 
-    """
-    print "Nothing at all"
-    pass
+#     """
+#     print "Nothing at all"
+#     pass
 
 
-def long_words(word_list):
-    """Return all words in input list that are longer than 4 characters.
+# def long_words(word_list):
+#     """Return all words in input list that are longer than 4 characters.
 
-        >>> long_words(["hello", "hey", "spam", "spam", "bacon", "bacon"])
-        ['hello', 'bacon', 'bacon']
+#         >>> long_words(["hello", "hey", "spam", "spam", "bacon", "bacon"])
+#         ['hello', 'bacon', 'bacon']
 
-        >>> long_words(["all", "are", "tiny"])
-        []
+#         >>> long_words(["all", "are", "tiny"])
+#         []
 
-    """
+#     """
 
-    return []
+#     return []
 
 
-def smallest_int(number_list):
-    """Find the smallest integer in a list of integers and return it.
+# def smallest_int(number_list):
+#     """Find the smallest integer in a list of integers and return it.
 
-        >>> smallest_int([-5, 2, -5, 7])
-        -5
+#         >>> smallest_int([-5, 2, -5, 7])
+#         -5
 
-    If the input list is empty, return None:
+#     If the input list is empty, return None:
 
-        >>> smallest_int([]) is None
-        True
+#         >>> smallest_int([]) is None
+#         True
 
-    """
+#     """
 
-    return 100
+#     return 100
 
 
-def largest_int(number_list):
-    """Find the largest integer in a list of integers and return it.
+# def largest_int(number_list):
+#     """Find the largest integer in a list of integers and return it.
 
-        >>> largest_int([-5, 2, -5, 7])
-        7
+#         >>> largest_int([-5, 2, -5, 7])
+#         7
 
-    If the input list is empty, return None:
+#     If the input list is empty, return None:
 
-        >>> largest_int([]) is None
-        True
+#         >>> largest_int([]) is None
+#         True
 
-    """
+#     """
 
-    return 0
+#     return 0
 
 
-def halvesies(number_list):
-    """Return list of numbers from input list, each divided by two.
+# def halvesies(number_list):
+#     """Return list of numbers from input list, each divided by two.
 
-        >>> halvesies([2, 6, -2])
-        [1.0, 3.0, -1.0]
+#         >>> halvesies([2, 6, -2])
+#         [1.0, 3.0, -1.0]
 
-    If any of the numbers are, make sure you don't round off the half:
+#     If any of the numbers are, make sure you don't round off the half:
 
-        >>> halvesies([1, 5])
-        [0.5, 2.5]
+#         >>> halvesies([1, 5])
+#         [0.5, 2.5]
 
-    """
+#     """
 
-    return []
+#     return []
 
 
-def word_lengths(word_list):
-    """Return the length of words in the input list.
+# def word_lengths(word_list):
+#     """Return the length of words in the input list.
 
-        >>> word_lengths(["hello", "hey", "hello", "spam"])
-        [5, 3, 5, 4]
+#         >>> word_lengths(["hello", "hey", "hello", "spam"])
+#         [5, 3, 5, 4]
 
-    """
+#     """
 
-    return []
+#     return []
 
 
-def sum_numbers(number_list):
-    """Return the sum of all of the numbers in the list.
+# def sum_numbers(number_list):
+#     """Return the sum of all of the numbers in the list.
 
-    Python has a built-in function, `sum()`, which already does this -- but for
-    this exercise, you should not use it.
+#     Python has a built-in function, `sum()`, which already does this -- but for
+#     this exercise, you should not use it.
 
-        >>> sum_numbers([1, 2, 3, 10])
-        16
+#         >>> sum_numbers([1, 2, 3, 10])
+#         16
 
-    Any empty list should return the sum of zero:
+#     Any empty list should return the sum of zero:
 
-        >>> sum_numbers([])
-        0
+#         >>> sum_numbers([])
+#         0
 
-    """
+#     """
 
-    return 0
+#     return 0
 
 
-def mult_numbers(number_list):
-    """Return product (result of multiplication) of the numbers in the list.
+# def mult_numbers(number_list):
+#     """Return product (result of multiplication) of the numbers in the list.
 
-        >>> mult_numbers([1, 2, 3])
-        6
+#         >>> mult_numbers([1, 2, 3])
+#         6
 
-    Obviously, if there is a zero in the input, the product will be zero:
+#     Obviously, if there is a zero in the input, the product will be zero:
 
-        >>> mult_numbers([10, 20, 0, 50])
-        0
+#         >>> mult_numbers([10, 20, 0, 50])
+#         0
 
-    As explained at http://en.wikipedia.org/wiki/Empty_product, if the list is
-    empty, the product should be 1:
+#     As explained at http://en.wikipedia.org/wiki/Empty_product, if the list is
+#     empty, the product should be 1:
 
-        >>> mult_numbers([])
-        1
+#         >>> mult_numbers([])
+#         1
 
-    """
-    return 0
+#     """
+#     return 0
 
 
-def join_strings(word_list):
-    """Return a string of all input strings joined together.
+# def join_strings(word_list):
+#     """Return a string of all input strings joined together.
 
-    Python ha a built-in method on lists, `join` -- but this exercise, you
-    should not use it.
+#     Python ha a built-in method on lists, `join` -- but this exercise, you
+#     should not use it.
 
-        >>> join_strings(["spam", "spam", "bacon", "balloonicorn"])
-        'spamspambaconballoonicorn'
+#         >>> join_strings(["spam", "spam", "bacon", "balloonicorn"])
+#         'spamspambaconballoonicorn'
 
-    For an empty list, you should return an empty string:
+#     For an empty list, you should return an empty string:
 
-        >>> join_strings([])
-        ''
+#         >>> join_strings([])
+#         ''
 
-    """
-    return ""
+#     """
+#     return ""
 
 
-def average(number_list):
-    """Return the average (mean) of the list of numbers given.
+# def average(number_list):
+#     """Return the average (mean) of the list of numbers given.
 
-        >>> average([2, 12, 3])
-        5.666666666666667
+#         >>> average([2, 12, 3])
+#         5.666666666666667
 
-    There is no defined answer if the list given is empty. It's fine if
-    this raises an error when given an empty list.
-    """
-    return 0
+#     There is no defined answer if the list given is empty. It's fine if
+#     this raises an error when given an empty list.
+#     """
+#     return 0
 
 
-##############################################################################
-# END OF SKILLS TEST; YOU CAN STOP HERE.
+# ##############################################################################
+# # END OF SKILLS TEST; YOU CAN STOP HERE.
 
 
-def intermediate_join_strings(list_of_words):
-    """Return a single string with each word from the input list
-    separated by a comma.
+# def intermediate_join_strings(list_of_words):
+#     """Return a single string with each word from the input list
+#     separated by a comma.
 
-    Do this with a list comprehension. See
-    https://docs.python.org/2/tutorial/datastructures.html#list-comprehensions
-    for more info.
+#     Do this with a list comprehension. See
+#     https://docs.python.org/2/tutorial/datastructures.html#list-comprehensions
+#     for more info.
 
-    >>> intermediate_join_strings(["Labrador", "Poodle", "French Bulldog"])
-    'Labrador, Poodle, French Bulldog'
+#     >>> intermediate_join_strings(["Labrador", "Poodle", "French Bulldog"])
+#     'Labrador, Poodle, French Bulldog'
 
-    As above, if the list given is empty, it's fine if this function
-    raises an error.
-    """
-    return ""
+#     As above, if the list given is empty, it's fine if this function
+#     raises an error.
+#     """
+#     return ""
 
 
-def adv_find_unique_long_words(my_string):
-    """Return a list of words that only appeared only once
-    within the input string and are at least 6 characters long.
+# def adv_find_unique_long_words(my_string):
+#     """Return a list of words that only appeared only once
+#     within the input string and are at least 6 characters long.
 
-    >>> adv_find_unique_long_words("I ate popcorn, more popcorn, nachos, kale, and coffee.")
-    ['nachos', 'coffee']
+#     >>> adv_find_unique_long_words("I ate popcorn, more popcorn, nachos, kale, and coffee.")
+#     ['nachos', 'coffee']
 
-    """
-    return []
+#     """
+#     return []
 
 
 ##############################################################################
